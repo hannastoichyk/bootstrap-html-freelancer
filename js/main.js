b197cc37f6a490022cff59c56a80ba14c57211e3
Hanna Stoichyk <hannastoichyk@gmail.com>
2023-11-27 10:43:26 +0100

    firstcommit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+/*Quando l’utente fa click sul bottone “send” del form, il sito deve calcolare l’ammontare del preventivo per le ore di lavoro richieste.
+Il prezzo orario per una commissione varia in questo modo:
+se la commissione riguarda lo sviluppo backend il prezzo orario è di 20.50€ l’ora
+se la commissione riguarda lo sviluppo frontend il prezzo orario è di 15.30€ l’ora
+se la commissione riguarda l’analisi progettuale di un progetto il prezzo orario è di 33.60€
+L’utente potrebbe decidere di utilizzare un codice promozionale tra i seguenti:
+YHDNU32
+JANJC63
+PWKCN25
+SJDPO96
+POCIE24
+Se l’utente inserisce un codice promozionale valido, ha diritto ad uno sconto del 25% sul prezzo finale. Se il codice inserito non è valido, il sito deve informare l’utente che il codice non è valido e il prezzo finale viene calcolato senza applicare sconti.
+// Il risultato del calcolo del prezzo finale deve essere visualizzato in “forma umana” (con 2 decimali e il simbolo dell’euro) in un apposito tag HTML appena sotto il bottone send.*/
+
+function offer(event) {
+  event.preventDefault();
+  let firstname = document.getElementById("firstname").value;
+  let lastname = document.getElementById("lastname").value;
+  let email = document.getElementById("email").value;
+
+ 
+}
